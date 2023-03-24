24d11bcac5fbd203dbdd28ea914c930abcfc7f17
Lochan2001 <patillochan1234@gmail.com>
2023-03-24 10:06:39 +0530

    prefinal

--- js/app.js
+++ js/app.js
@@ -18,3 +18,19 @@ $(".grid").isotope({
     layoutMode: "fitRows",
     transitionDuration: "0.6s",
   });
+
+
+
+
+var tablinks =document.getElementsByClassName("tab-links");
+var tabcontents =document.getElementsByClassName("tab-contents");
+function opentab(tabname){
+  for(tablink of tablinks){
+    tablink.classList.remove("active-link");
+  }
+  for(tabcontent of tabcontents){
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab")
+}
